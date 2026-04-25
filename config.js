6726250369a273285a25a7d57232914a9cca0781
northsellermedia <northsellermedia@gmail.com>
2026-04-25 22:12:12 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+window.OFB_CONFIG = {
+  CHANNEL_URL: 'https://max.ru/ofb24news',
+
+  ENDPOINTS: {
+    profile: 'ofb-api-profile',
+    teamSearch: 'ofb-api-team-search',
+    teamCard: 'ofb-api-team-card',
+    subscribe: 'ofb-api-subscribe',
+    updateFlags: 'ofb-api-update-flags',
+    unsubscribe: 'ofb-api-unsubscribe',
+
+    teamFixtures: 'ofb-api-team-fixtures',
+    teamProfile: 'ofb-api-team-profile',
+    teamSquad: 'ofb-api-team-squad',
+    teamTransfers: 'ofb-api-team-transfers',
+    teamSeasonStats: 'ofb-api-team-season-stats'
+  }
+};
