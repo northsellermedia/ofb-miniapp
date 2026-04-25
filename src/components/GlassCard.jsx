919895a69be6086b1e01bcdb05475365c6e32e75
northsellermedia <northsellermedia@gmail.com>
2026-04-25 17:54:31 +0300

    Delete src directory

--- src/components/GlassCard.jsx
+++
@@ -1,7 +0,0 @@
-export default function GlassCard({ children, className = '', onClick }) {
-  return (
-    <section className={`glass-card ${className}`} onClick={onClick}>
-      {children}
-    </section>
-  );
-}
--- src/components/LoadingView.jsx
+++
@@ -1,8 +0,0 @@
-export default function LoadingView({ text = 'Загружаем данные' }) {
-  return (
-    <div className="loading-view">
-      <div className="loader" />
-      <p>{text}</p>
-    </div>
-  );
-}
